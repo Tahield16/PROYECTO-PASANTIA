1c69cb6d4191fa43287881c05b5e0ea6037e10fa
Tahiel Martín Devoto <tahielmartind16@gmail.com>
2025-09-15 14:38:27 -0300

    refactor: Restructure of header component and style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,27 @@
-import '../assets'
-export const Header=()=>{
-    return(
-        <header>
-             <div className="logo-container">
+import { Link } from "@tanstack/react-router";
+import styles from "./header.module.scss";
+// import Logo from '/assets/logoDTV.svg?react';
 
-             </div>
-        </header>
-    )
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logoContainer}>
+        <img src="/assets/logoDVT.svg" alt="Logo de Devoted To Videogames" />
+        <h1 className={styles.title}>Devoted To Videogames</h1>
+      </div>
+   
+        <nav className={styles.navContainer}>
+          <Link to="/" className="[&.active]:text-yellow-400">
+            Inicio
+          </Link>
+          <Link to="/crear" className="[&.active]:text-yellow-400">
+            Crear
+          </Link>
+          <Link to="/favoritos" className="[&.active]:text-yellow-400">
+            Favoritos
+          </Link>
+        </nav>
+      
+    </header>
+  );
 }
