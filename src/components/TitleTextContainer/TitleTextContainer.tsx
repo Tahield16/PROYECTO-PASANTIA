03e62f7250a70947ba05bfec6c5e4d4eb144d02a
Tahiel Martín Devoto <tahielmartind16@gmail.com>
2025-09-18 09:30:34 -0300

    feat: created TitleTextContainer component and added responsive design to it

--- src/components/TitleTextContainer/TitleTextContainer.tsx
+++ src/components/TitleTextContainer/TitleTextContainer.tsx
@@ -1,12 +1,15 @@
+import styles from './TitleTextContainer.module.scss'
 interface TitleTextType{
     title:string,
-    text:string,
-    highlited:string
+    text:string | null,
+    highlited:string | null
 }
-const TitleTextContainer=({title,text,highlited}:TitleTextType)=>{
+export const TitleTextContainer=({title,text,highlited}:TitleTextType)=>{
     return(
         <>
-         <h1 className="">{title}</h1>
+         <h1 className={styles.title}>{title}</h1>
+         {text!=null && highlited!=null?<p className={styles.text}>{text} <span className={styles.highlite}>{highlited}</span></p>:<p></p>}
+         
         </>
     )
 
