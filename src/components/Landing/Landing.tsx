af304d768f615425637909ad12be0dee9a6f2bf8
Tahiel Martín Devoto <tahielmartind16@gmail.com>
2025-09-29 15:26:28 -0300

    feat: added filterContainer to Landig page

--- src/components/Landing/Landing.tsx
+++ src/components/Landing/Landing.tsx
@@ -1,6 +1,7 @@
 import { useGamesStore } from "../../store/gamesStore";
 import { GameCardLink } from "../CardGame/CardLink/GameCardLink";
 import { TitleTextContainer } from "../TitleTextContainer/TitleTextContainer";
+import { FiltersContainer } from "../Filters/FiltersContainer";
 import styles from './Landing.module.scss'
 const Landing=()=>{
    const { games } = useGamesStore();
@@ -8,6 +9,7 @@ const Landing=()=>{
   return (
     <main className={styles.landingContainer}>
         <TitleTextContainer title="Biblioteca" text="Encuentra, monitorea y consagra tus juegos favoritos en una" highlited="biblioteca sagrada" />
+        <FiltersContainer></FiltersContainer>
       <section className={styles.gamesContainer}>
         {games.map((game) => (
           <GameCardLink key={game._id} {...game} />
