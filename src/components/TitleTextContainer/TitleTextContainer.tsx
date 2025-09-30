8026996b20edc339260fa951dbe58d7cf679aa7a
Tahiel Martín Devoto <tahielmartind16@gmail.com>
2025-09-30 11:05:52 -0300

    feat: made title optional

--- src/components/TitleTextContainer/TitleTextContainer.tsx
+++ src/components/TitleTextContainer/TitleTextContainer.tsx
@@ -1,14 +1,14 @@
 import styles from './TitleTextContainer.module.scss'
 interface TitleTextType{
-    title:string,
-    text:string | null,
-    highlited:string | null
+    title:string | undefined,
+    text:string | undefined,
+    highlited:string | undefined
 }
 export const TitleTextContainer=({title,text,highlited}:TitleTextType)=>{
     return(
         <>
          <h1 className={styles.title}>{title}</h1>
-         {text!=null && highlited!=null?<p className={styles.text}>{text} <span className={styles.highlite}>{highlited}</span></p>:<p></p>}
+         {text!=undefined && highlited!=undefined?<p className={styles.text}>{text} <span className={styles.highlite}>{highlited}</span></p>:<p></p>}
          
         </>
     )
