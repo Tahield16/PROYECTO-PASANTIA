de797f873cc5b6772379cfbb1f07907f7df92f7c
Tahiel Martín Devoto <tahielmartind16@gmail.com>
2025-10-14 10:22:30 -0300

    feat: change route of the public logo page on the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import { Link } from "@tanstack/react-router";
 import styles from "./header.module.scss";
-// import Logo from '/assets/logoDTV.svg?react';
+
 
 export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
-        <img src="/assets/logoDVT.svg" alt="Logo de Devoted To Videogames" />
+        <img src='assets/logoDVT.svg' alt="Logo de Devoted To Videogames" />
         <h1 className={styles.title}>Devoted To Videogames</h1>
       </div>
    
