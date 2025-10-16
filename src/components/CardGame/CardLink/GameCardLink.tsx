45be613e8f6a59861e07061a76ec8e44ddd6588f
Tahiel Martín Devoto <tahielmartind16@gmail.com>
2025-10-16 10:30:57 -0300

    fix: update edit link background color and refactor Card import in GameCardLink component

--- src/components/CardGame/CardLink/GameCardLink.tsx
+++ src/components/CardGame/CardLink/GameCardLink.tsx
@@ -1,13 +1,21 @@
 import { Link } from "@tanstack/react-router";
 import type { Game } from "../../../types/gameType";
-import { Card } from "../Cards/Cards";
+import { Card } from "../Cards/Card/Card";
 import styles from "./GameCardLink.module.scss";
 export const GameCardLink = (game: Game) => {
   const { _id } = game;
   return (
     <article className={styles.cardContainer}>
       <Link to="/$gameId" params={{ gameId: String(_id) }}>
-        <Card _id={_id} name={game.name} img={game.img} description={game.description} favorite={game.favorite} rating={game.rating} key={_id} />
+        <Card
+          _id={_id}
+          name={game.name}
+          backgroundImage={game.backgroundImage}
+          description={game.description}
+          favorite={game.favorite}
+          rating={game.rating}
+          key={_id}
+        />
       </Link>
       <Link
         to="/$gameId/edit"
@@ -17,7 +25,6 @@ export const GameCardLink = (game: Game) => {
         <img
           src="/assets/edit-3-svgrepo-com.svg"
           alt="Edit logo, click it to edit this game."
-          
         />
       </Link>
     </article>
