05111e8c68ef17a0d12c2b792bdebbc1a10f1d79
Tahiel Martín Devoto <tahielmartind16@gmail.com>
2025-11-06 10:41:53 -0300

    style: erase unnecessary code comments

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,5 @@
-// import { createFileRoute} from "@tanstack/react-router";
-// export const Route=createFileRoute('/')({
-//     component:()=><h1>Hola Bienvenido a inicio</h1>
-// })
 import { createFileRoute } from "@tanstack/react-router";
-// import '../styles/_base.scss'
-// import App from "../App";
+
 import Landing from "../components/Landing/Landing";
 export const Route = createFileRoute("/")({
   component: Landing,
