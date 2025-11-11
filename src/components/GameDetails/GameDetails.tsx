b7dd4ac0e6ba7acf667d1127bee7afa1a74467c2
Tahiel Martín Devoto <tahielmartind16@gmail.com>
2025-11-11 13:23:54 -0300

    feat: added API functionality and styled GameDetails

--- src/components/GameDetails/GameDetails.tsx
+++ src/components/GameDetails/GameDetails.tsx
@@ -1,48 +1,22 @@
-import { useState } from "react";
-
-import type { Game } from "../../types/gameType";
+import { useSelectedGameStore } from "../../store/selectedGameStore";
 import { RatingBar } from "../RatingBar/RatingBar";
 import { TitleTextContainer } from "../TitleTextContainer/TitleTextContainer";
 import styles from "./GameDetails.module.scss";
-import { useGamesStore } from "../../store/gamesStoreApi";
 
-interface GameDetailsProps {
-  id: Game["id"];
-}
-export const GameDetails = ({ id }: GameDetailsProps) => {
-  const { games } = useGamesStore();
-  const selectedGame = games.find((game) => game.id === id);
-  const maxThumbnails: number | undefined = selectedGame?.shortScreenshots
-    ?.length
-    ? selectedGame.shortScreenshots.length >= 4
-      ? 4
-      : selectedGame.shortScreenshots.length
-    : undefined;
-  const [mainImgSource, setMainImgSource] = useState(
-    selectedGame?.background_image
-  );
-  const [thumbnails, setThumbnails] = useState(
-    selectedGame?.shortScreenshots
-      ?.slice(0, maxThumbnails)
-      .filter((s) => s.image !== mainImgSource) ?? []
-  );
+export const GameDetails = () => {
 
-  // const handleClickImg = (screenshotImg: string) => {
-  //   if (!mainImgSource || mainImgSource === screenshotImg) return;
-  //   setThumbnails((prev) => (
-  //     const newThumbs = prev.map((screen) => (
-  //       const newSource=screen.image==screenshotImg ? mainImgSource:screen.image
-  //     ));
-  //   ));
-  //   // Intercambia la imagen principal con la seleccionada
-  //   setMainImgSource(screenshotImg);
-  // };
+  const { game } = useSelectedGameStore();
+
+
+  console.log(game);
+
+    const pcRequirments=game?.platforms?.find((p)=>p.platform.id===4);
   return (
     <main className={styles.detailsContainer}>
       <div className={styles.centeredContent}>
         <section className={styles.TitleDescriptionContainer}>
           <TitleTextContainer
-            title={selectedGame?.name}
+            title={game?.name}
             text={undefined}
             highlited={undefined}
           />
@@ -50,40 +24,26 @@ export const GameDetails = ({ id }: GameDetailsProps) => {
             <span className={styles.relaseHighlight}>
               Fecha de lanzamiento:
             </span>{" "}
-            {selectedGame?.released}
+            {game?.released}
           </p>
         </section>
 
         <section className={styles.galleryContainer}>
           <img
-            src={mainImgSource}
-            alt={`Selected Image of ${selectedGame?.name}`}
+            src={game?.background_image}
+            alt={`Selected Image of ${game?.name}`}
             className={styles.mainImg}
           />
           <div className={styles.thumbnailsContainer}>
-            {thumbnails.map((screenshot, i) => (
-              <div key={i}>
-                <img
-                  // () => handleClickImg(screenshot.image)
-                  onClick={() => {}}
-                  src={screenshot.image}
-                  alt={`${selectedGame?.name}_gameplay_ss`}
-                  className={styles.screenshot}
-                />
-              </div>
-            ))}
+           <img src={game?.background_image_additional} alt={`Imagen alternativa de ${game?.name}`} className={styles.screenshot} />
+          <RatingBar ratings={game?.ratings} />
           </div>
         </section>
         <section className={styles.ratingGameContainer}>
-          <RatingBar ratings={selectedGame?.ratings} />
           <div className={styles.platformsContainer}>
             <p className={styles.textHighlight}>Conseguilo en: </p>
-            {selectedGame?.stores?.map(({ url, store }) => (
-              <a
-                href={url}
-                target="_blank"
-                key={`${selectedGame.id}-${store.name}`}
-              >
+            {game?.stores?.map(({ url, store }) => (
+              <a href={url} target="_blank" key={`${game.id}-${store.name}`}>
                 <div className={styles.platformWrapper}>
                   <img
                     src={store.image_background}
@@ -98,50 +58,48 @@ export const GameDetails = ({ id }: GameDetailsProps) => {
         </section>
 
         <section className={styles.descriptionWrapper}>
-          <div className={styles.AboutContent}>
             <TitleTextContainer
               title="Acerca de: "
               highlited={undefined}
               text={undefined}
             ></TitleTextContainer>
-            <p className={styles.gameDescription}>
-              {selectedGame?.description}
-            </p>
+          <div className={styles.aboutContent}>
+            <p className={styles.gameDescription}>{game?.description_raw}</p>
           </div>
         </section>
         <section className={styles.featuresContainer}>
           <div className={styles.featureItem}>
-            <div className={styles.tagsContainer}>
+            <div className={`${styles.tagsContainer} ${styles.feature}`}>
               <p>
                 {" "}
                 <span className={styles.textHighlight}>Tags: </span>
               </p>
-              {selectedGame?.tags?.map((tag) => (
+              {game?.tags?.map((tag) => (
                 <div
                   className={styles.genreWrapper}
-                  key={`${selectedGame.id}-${tag.name}`}
+                  key={`${game.id}-${tag.name}`}
                 >
                   <span>{tag.slug}</span>
                 </div>
               ))}
-              {selectedGame?.tags?.map((tag) => (
+              {game?.tags?.map((tag) => (
                 <div
                   className={styles.genreWrapper}
-                  key={`${selectedGame.id}-${tag.name}`}
+                  key={`${game.id}-${tag.name}`}
                 >
                   <span>{tag.slug}</span>
                 </div>
               ))}
             </div>
-            <div className={styles.genresContainer}>
+            <div className={`${styles.genresContainer }  ${styles.feature}`}>
               <p>
                 {" "}
                 <span className={styles.textHighlight}>Generos: </span>
               </p>
-              {selectedGame?.genres?.map((genre) => (
+              {game?.genres?.map((genre) => (
                 <div
                   className={styles.genreWrapper}
-                  key={`${selectedGame.id}-${genre.name}`}
+                  key={`${game.id}-${genre.name}`}
                 >
                   <span>{genre.slug}</span>
                 </div>
@@ -149,42 +107,38 @@ export const GameDetails = ({ id }: GameDetailsProps) => {
             </div>
           </div>
           <div className={styles.featureItem}>
-            <div className={styles.publishersContainer}>
+            <div className={`${styles.publishersContainer} ${styles.feature}`}>
               <p>
                 <span className={styles.textHighlight}>Publishers : </span>
-                {selectedGame?.publishers?.map(
-                  (publisher, index, publishers) => (
-                    <span key={index} className={styles.publisher}>
-                      {publisher}
-                      {index < publishers.length - 1 && <span>, </span>}
-                    </span>
-                  )
-                )}
+                {game?.publishers?.map((publisher, index, publishers) => (
+                  <span key={index} className={styles.publisher}>
+                    {publisher.slug}
+                    {index < publishers.length - 1 && <span>, </span>}
+                  </span>
+                ))}
               </p>
             </div>
-            <div className={styles.developersContainer}>
+            <div className={`${styles.developersContainer}  ${styles.feature}`}>
               <p>
                 <span className={styles.textHighlight}>Developers : </span>
-                {selectedGame?.developedBy?.map((developer, index) => (
+                {game?.developers?.map((developer, index) => (
                   <span key={index} className={styles.developer}>
-                    {developer}
-                    {index < selectedGame.developedBy.length - 1 && (
-                      <span>,</span>
-                    )}
+                    {developer.slug}
+                    {index < game.developers.length - 1 && <span>,</span>}
                   </span>
                 ))}
               </p>
             </div>
           </div>
           <div className={styles.featureItem}>
-            <div className={styles.requirementsContainer}>
+            <div className={`${styles.requirementsContainer} ${styles.feature}`}>
               <p className={`${styles.text} ${styles.requirement}`}>
-                {selectedGame?.platforms[0]?.requirements.minimum}
+                {pcRequirments?.requirements.minimum}
               </p>
             </div>
-            <div className={styles.requirementsContainer}>
+            <div className={`${styles.requirementsContainer} ${styles.feature}`}>
               <p className={styles.requirement}>
-                {selectedGame?.platforms[0]?.requirements.recommended}
+                {pcRequirments?.requirements.recommended}
               </p>
             </div>
           </div>
